52c4a6538b123e006dcd35cde8bb5e4e6a6d55de
ROFLERGG <alexeysuper0@gmail.com>
2024-04-06 04:46:41 +0300

    ...

--- src/components/ui/menu.jsx
+++ src/components/ui/menu.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
+
 const Menu = () => {
   return (
-    <div className="fixed w-screen h-screen bg-primary"></div>
+    <div className={`fixed translate-x-full w-screen h-screen bg-primary`}></div>
   )
 }
 
