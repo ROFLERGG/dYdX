d589fc8e6b743146136f449a67715dbf2baa4953
ROFLERGG <alexeysuper0@gmail.com>
2024-04-10 21:47:51 +0300

    ...

--- src/components/ui/buttons.jsx
+++ src/components/ui/buttons.jsx
@@ -35,8 +35,8 @@ const buttonVariants = cva('cursor-pointer rounded-lg', {
 })
 
 
-const Button = ({ link, btn, bg, text, href, className, ...rest }) => {
-  return <a href={href ? href : undefined} className={buttonVariants({ link, btn, bg, text, className })} {...rest}/>
+const Button = ({ link, btn, bg, text, href, className }) => {
+  return <a href={href ? href : undefined} className={buttonVariants({ link, btn, bg, text, className })}/>
 }
 
 export default Button
