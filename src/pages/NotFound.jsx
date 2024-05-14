e119103dca2909e56784e51f9aa697530a3900bd
ROFLERGG <alexeysuper0@gmail.com>
2024-05-14 21:49:12 +0300

    some fixes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,10 +6,12 @@ const NotFound = () => {
     <Layout>
       <Content />
       <div className="flex flex-col justify-center flex-1 z-10">
-        <div className="flex justify-center">
-          <div className="max-w-[600px] flex flex-1 flex-col items-center space-y-6">
-            <h1 className="hero text-white-500">404</h1>
-            <h2 className="heading-lg text-white-500">Page not found :(</h2>
+        <div className="container">
+          <div className="flex justify-center">
+            <div className="max-w-[600px] flex flex-1 flex-col items-center space-y-6">
+              <h1 className="hero text-white-500">404</h1>
+              <h2 className="heading-lg text-white-500">Page not found :(</h2>
+            </div>
           </div>
         </div>
       </div>
